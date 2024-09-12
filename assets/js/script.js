c05f1afc6331f0a9b36810dcd5ed0b50b7b69dbe
Ranjith Natarajan <132538605+Ranjith1110@users.noreply.github.com>
2024-09-12 16:28:10 +0530

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,7 @@
-const header = document.querySelector("#navbar");
-
-window.addEventListener("scroll", function() {
-    navbar.classList.toggle("sticky", window.scrollY > 200);
-});
-
 let menu = document.querySelector('.menu-icon');
 let slider = document.querySelector('.slider');
-let icon = document.querySelectorAll('.slider li');
+let icon = document.querySelectorAll('.slider a');
 menu.onclick = () => {
-    // menu.classList.toggle('menu-icon');
     slider.classList.toggle('active');
 }
 
